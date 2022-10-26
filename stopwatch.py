9372c309f8d43f6125979670d5b5827d9024d497
lukexyz <lukedwoods@gmail.com>
2022-10-26 10:18:19 +0100

    add stopwatch app from tutorial

---
+++ stopwatch.py
@@ -0,0 +1,20 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+class StopwatchApp(App):
+    "Textual app from the tutorial docs."
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+if __name__ == "__main__":
+    app = StopwatchApp()
+    app.run()
